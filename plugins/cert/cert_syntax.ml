e7052085e0ebdf01f7629469a98be47a1e67f161
Quentin Garchery <garchery.quentin@gmail.com>
2019-02-26 18:33:46 +0100

    tac to trans

--- plugins/cert/cert_syntax.ml
+++ plugins/cert/cert_syntax.ml
@@ -7,10 +7,10 @@ open Task
 type ident = Ident.ident
 type cterm = CTapp of ident | CTand of cterm * cterm
 type ctask = { cctxt : (ident * cterm) list; cgoal : cterm }
-type certif = Stop
+type certif = Skip
             | Axiom of ident
             | Split of certif * certif
-type certified_trans = task -> task list * certif
+type ctrans = task -> task list * certif
 
 (* Translating Why3 tasks to simplified certificate tasks *)
 let rec translate_term (t : term) : cterm =
@@ -46,7 +46,7 @@ let translate_task (t : task) =
 exception Certif_verif_failed
 let rec check_certif ({cctxt = c; cgoal = t} as ct) (cert : certif)  : ctask list =
   match cert with
-    | Stop -> [ct]
+    | Skip -> [ct]
     | Axiom id -> begin match List.assoc_opt id c with
                   | Some t' when t = t' -> []
                   | _ -> raise Certif_verif_failed end
@@ -55,14 +55,14 @@ let rec check_certif ({cctxt = c; cgoal = t} as ct) (cert : certif)  : ctask lis
                                                 check_certif {ct with cgoal = t2} c2
                           | _ -> raise Certif_verif_failed end
 
-(* Creates a certified tactic from a tactic that produces certificates *)
-let cert_tactic tac task =
-  try let ltask, step = tac task in
+(* Creates a certified transformation from a ctrans *)
+let ctrans_to_trans tr task =
+  try let ltask, step = tr task in
       let ctask = translate_task task in
       let lctask = check_certif ctask step in
       if lctask = List.map translate_task ltask then ltask
       else failwith "Certif verification failed."
-  with e -> raise (Trans.TransFailure ("cert_tactic", e))
+  with e -> raise (Trans.TransFailure ("cert_trans", e))
 
 (* Assumption with certificate *)
 let assumption_decl g (d : decl) =
@@ -84,15 +84,15 @@ let rec assumption_ctxt g = function
       | None -> assumption_ctxt g p end
   | None -> raise Not_found
 
-let assumption_certif (t : task) : task list * certif =
+let assumption_ctrans t =
   let g = try task_goal_fmla t
           with GoalNotFound -> invalid_arg "Cert_syntax.assumption_task" in
   let _, t' = task_separate_goal t in
   try let h = assumption_ctxt g t' in [], Axiom h
-  with Not_found -> [t], Stop
+  with Not_found -> [t], Skip
 
 (* Split with certificate *)
-let split_certif (t : task) : task list * certif =
+let split_ctrans t =
   let pr, g = try task_goal t, task_goal_fmla t
               with GoalNotFound -> invalid_arg "Cert_syntax.split_certif" in
   let _, c = task_separate_goal t in
@@ -100,50 +100,48 @@ let split_certif (t : task) : task list * certif =
   | Tbinop (Tand, f1, f2) ->
       let t1 = Task.add_decl c (create_prop_decl Pgoal pr f1) in
       let t2 = Task.add_decl c (create_prop_decl Pgoal pr f2) in
-      [t1;t2], Split (Stop, Stop)
-  | _ -> [t], Stop
+      [t1;t2], Split (Skip, Skip)
+  | _ -> [t], Skip
 
 (* Compose *)
-let compose_certif tr1 tr2 task =
+let compose_ctrans (tr1 : ctrans) (tr2 : ctrans) : ctrans = fun task ->
   let ts, c = tr1 task in
   let rec fill c ts = match c with
-      | Stop -> begin match ts with
+      | Skip -> begin match ts with
                 | [] -> assert false
                 | t::ts -> let l2, c2 = tr2 t in
                            c2, l2, ts end
       | Axiom _ -> c, [], ts
       | Split (c1, c2) -> let c1, l1, ts1 = fill c1 ts in
                           let c2, l2, ts2 = fill c2 ts1 in
-                          Split (c1, c2), l1 @ l2, ts2 in
+                          c, l1 @ l2, ts2 in
   let c, l, ts = fill c ts in
   assert (ts = []);
   l, c
 
-let split_assumption_certif = compose_certif split_certif assumption_certif
+let split_assumption_ctrans = compose_ctrans split_ctrans assumption_ctrans
 
-let compose2 tac tac1 tac2 task =
-  match tac task with
-    | [task1; task2] -> tac1 task1 @ tac2 task2
+let compose2 tr tr1 tr2 task =
+  match tr task with
+    | [task1; task2] -> tr1 task1 @ tr2 task2
     | _ -> failwith "wrong arity"
 
 
-(* Certified tactic *)
-let split_cert = cert_tactic split_certif
-let assumption_cert = cert_tactic assumption_certif
-let try_assumption_cert = try_tac assumption_cert
+(* Certified transformations *)
+let split_trans = ctrans_to_trans split_ctrans
+let assumption_trans = ctrans_to_trans assumption_ctrans
 
 let intuition_cert_start =
-  compose2 split_cert assumption_cert assumption_cert
-let split_assumption_cert = cert_tactic split_assumption_certif
+  compose2 split_trans assumption_trans assumption_trans
+
+let split_assumption_cert = ctrans_to_trans split_assumption_ctrans
 
 let () =
-  Trans.register_transform_l "assumption_cert" (Trans.store assumption_cert)
-    ~desc:"A certified version of coq tactic assumption";
-  Trans.register_transform_l "split_cert" (Trans.store split_cert)
-    ~desc:"A certified version of coq tactic split";
-  Trans.register_transform_l "try_assumption_cert" (Trans.store try_assumption_cert)
-    ~desc:"A certified version of coq tactic split";
+  Trans.register_transform_l "assumption_cert" (Trans.store assumption_trans)
+    ~desc:"A certified version of coq tactic [assumption]";
+  Trans.register_transform_l "split_cert" (Trans.store split_trans)
+    ~desc:"A certified version of (simplified) coq tactic [split]";
   Trans.register_transform_l "intuition_cert_start" (Trans.store intuition_cert_start)
-    ~desc:"A certified version of tactic intuition";
+    ~desc:"A certified version of (simplified) coq tactic [intuition]";
   Trans.register_transform_l "split_assumption_cert" (Trans.store split_assumption_cert)
-    ~desc:"A certified version of coq tactic split; assumption"
+    ~desc:"A certified version of (simplified) coq tactic [split; assumption]"
