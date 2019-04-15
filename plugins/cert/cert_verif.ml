f1114a8731d4664af1745e46815d3557bda2ffb0
Quentin Garchery <garchery.quentin@gmail.com>
2019-04-15 15:19:34 +0200

    binop from Term

--- plugins/cert/cert_verif.ml
+++ plugins/cert/cert_verif.ml
@@ -1,6 +1,7 @@
 open Why3
 
 open Ident
+open Term (* only for binop *)
 open Cert_syntax
 
 
@@ -34,14 +35,15 @@ let rec ct_bv_subst k u t = match t with
 let ct_open t u = ct_bv_subst 0 u t
 
 (* checks if the term is locally closed *)
-let locally_closed t =
+let locally_closed =
   let di = id_register (id_fresh "dummy_locally_closed_ident") in
   let rec term = function
     | CTbvar _ -> false
     | CTfvar _ -> true
     | CTbinop (_, t1, t2) -> term t1 && term t2
-    | CTforall t -> term (ct_open t (CTfvar di)) in
-  term t
+    | CTforall t -> term (ct_open t (CTfvar di))
+  in
+  term
 
 (* free variable substitution *)
 let rec ct_fv_subst z u t = match t with
@@ -105,13 +107,13 @@ let rec check_rewrite_term tl tr t path =
 
 let check_rewrite cta rev h g path : ctask list =
   let rec introduce acc = function
-    | CTbinop (CTimplies, t1, t2) -> introduce (t1::acc) t2
+    | CTbinop (Timplies, t1, t2) -> introduce (t1::acc) t2
     | t -> acc, t in
   let lp, tl, tr =
     let ct, pos = find_ident h cta in
     if pos then verif_failed "Can't use goal as an hypothesis to rewrite" else
       match introduce [] ct with
-      | lp, CTbinop (CTiff, t1, t2) -> if rev then lp, t1, t2 else lp, t2, t1
+      | lp, CTbinop (Tiff, t1, t2) -> if rev then lp, t1, t2 else lp, t2, t1
       | _ -> verif_failed "Can't find the hypothesis used to rewrite" in
   let rewrite_decl h (te, pos) =
     if id_equal h g
@@ -140,7 +142,7 @@ let rec check_certif cta (r, g : certif) : ctask list =
     | Split (c1, c2) ->
         let t, pos = find_ident g cta in
         begin match t, pos with
-        | CTbinop (CTand, t1, t2), true | CTbinop (CTor, t1, t2), false ->
+        | CTbinop (Tand, t1, t2), true | CTbinop (Tor, t1, t2), false ->
             let cta1 = Mid.add g (t1, pos) cta in
             let cta2 = Mid.add g (t2, pos) cta in
             check_certif cta1 c1 @ check_certif cta2 c2
@@ -148,34 +150,34 @@ let rec check_certif cta (r, g : certif) : ctask list =
     | Unfold c ->
         let t, pos = find_ident g cta in
         begin match t with
-        | CTbinop (CTiff, t1, t2) ->
-            let imp_pos = CTbinop (CTimplies, t1, t2) in
-            let imp_neg = CTbinop (CTimplies, t2, t1) in
-            let destruct_iff = CTbinop (CTand, imp_pos, imp_neg), pos in
+        | CTbinop (Tiff, t1, t2) ->
+            let imp_pos = CTbinop (Timplies, t1, t2) in
+            let imp_neg = CTbinop (Timplies, t2, t1) in
+            let destruct_iff = CTbinop (Tand, imp_pos, imp_neg), pos in
             let cta = Mid.add g destruct_iff cta in
             check_certif cta c
-        | _ -> verif_failed "Not decodable" end
+        | _ -> verif_failed "Nothing to unfold" end
     | Destruct (h1, h2, c) ->
         let t, pos = find_ident g cta in
         begin match t, pos with
-        | CTbinop (CTand, t1, t2), false | CTbinop (CTor, t1, t2), true ->
+        | CTbinop (Tand, t1, t2), false | CTbinop (Tor, t1, t2), true ->
             let cta = Mid.remove g cta
                       |> Mid.add h1 (t1, pos)
                       |> Mid.add h2 (t2, pos) in
             check_certif cta c
-        | _ -> verif_failed "Not destructible"  end
+        | _ -> verif_failed "Nothing to destruct"  end
     | Dir (d, c) ->
         let t, pos = find_ident g cta in
         begin match t, d, pos with
-        | CTbinop (CTor, t, _), Left, true | CTbinop (CTor, _, t), Right, true
-        | CTbinop (CTand, t, _), Left, false | CTbinop (CTand, _, t), Right, false ->
+        | CTbinop (Tor, t, _), Left, true | CTbinop (Tor, _, t), Right, true
+        | CTbinop (Tand, t, _), Left, false | CTbinop (Tand, _, t), Right, false ->
           let cta = Mid.add g (t, pos) cta in
           check_certif cta c
         | _ -> verif_failed "Can't follow a direction" end
     | Intro (h, c) ->
         let t, pos = find_ident g cta in
         begin match t, pos with
-        | CTbinop (CTimplies, f1, f2), true ->
+        | CTbinop (Timplies, f1, f2), true ->
             let cta = Mid.add h (f1, false) cta
                       |> Mid.add g (f2, true) in
             check_certif cta c
